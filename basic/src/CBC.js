49f6ef51d98e614dd156b0fe68d79b8e774b01e9
keerthana <keerthisvr1127@gmail.com>
2023-11-08 10:52:44 +0530

    base=app

---
+++ basic/src/CBC.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class CBC extends Component {
+  render() {
+    return (
+      <div>
+        <p>from cbc </p>
+      </div>
+    )
+  }
+}
